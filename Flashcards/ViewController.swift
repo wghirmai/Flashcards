cb0410cdf56cdb56cefa5dac277bf4ee9b331b68
Winta Ghirmai <wintaghirmai@wintas-mbp.middlebury.edu>
2018-10-13 20:55:38 -0400

    simple cashcards lab done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backlabel: UILabel!
+    
+    @IBOutlet weak var frontlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        
+        if (frontlabel.isHidden == true){
+            frontlabel.isHidden = false;
+        }else{
+            frontlabel.isHidden = true;
+        
+        }
+    }
+    
 }
 
